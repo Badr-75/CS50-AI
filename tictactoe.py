1b98053d53722c3bc035c646ccee6b004112da91
Badr-75 <75635083+Badr-75@users.noreply.github.com>
2025-03-08 11:58:31 -0600

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,158 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    # Count the number of X's and O's on the board
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    
+    # If X's count is greater than O's count, it's O's turn
+    if x_count > o_count:
+        return O
+    # Otherwise, it's X's turn
+    else:
+        return X    
+    
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i, j))
+    return possible_actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    if board[action[0]][action[1]] is not EMPTY:
+        raise ValueError("Invalid action: cell is not empty")
+
+    new_board = copy.deepcopy(board)
+    new_board[action[0]][action[1]] = player(board)
+    return new_board    
+    
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    # Check rows for a winner
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] is not None:
+            return row[0]
+    
+    # Check columns for a winner
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] is not None:
+            return board[0][col]
+    
+    # Check diagonals for a winner
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
+    
+    # If no winner, return None
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    # If there is a winner, the game is over
+    if winner(board) is not None:
+        return True
+    
+    # If there are no empty spaces left, the game is over
+    for row in board:
+        if EMPTY in row:
+            return False
+    
+    return True
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    if current_player == X:
+        value, move = max_value(board)
+    else:
+        value, move = min_value(board)
+
+    return move
+
+
+def max_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = -math.inf
+    best_move = None
+    for action in actions(board):
+        min_val, _ = min_value(result(board, action))
+        if min_val > v:
+            v = min_val
+            best_move = action
+    return v, best_move
+
+
+def min_value(board):
+    if terminal(board):
+        return utility(board), None
+
+    v = math.inf
+    best_move = None
+    for action in actions(board):
+        max_val, _ = max_value(result(board, action))
+        if max_val < v:
+            v = max_val
+            best_move = action
+    return v, best_move
